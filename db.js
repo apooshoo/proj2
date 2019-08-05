0d8730982e363159fbb9e82564f14ee5c17b562f
Jonathan <jonathanbek93@gmail.com>
2019-08-05 15:52:37 +0800

    blabla

--- db.js
+++ db.js
@@ -1,15 +1,45 @@
 const pg = require('pg');
 const url = require('url');
 
-var configs;
+// var configs;
 
-configs = {
-        user: 'apooshoo',
-        password: 'neilgaiman1',
-        host: '127.0.0.1',
-        database: 'proj2',
-        port: 5432
-    };
+// configs = {
+//         user: 'apooshoo',
+//         password: 'neilgaiman1',
+//         host: '127.0.0.1',
+//         database: 'proj2',
+//         port: 5432
+//     };
+
+//check to see if we have this heroku environment variable
+if( process.env.DATABASE_URL ){
+
+  //we need to take apart the url so we can set the appropriate configs
+
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  //make the configs object
+  var configs = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+  };
+
+}else{
+
+  //otherwise we are on the local network
+  var configs = {
+      user: 'apooshoo',
+      password: 'neilgaiman1'
+      host: '127.0.0.1',
+      database: 'proj2',
+      port: 5432
+  };
+}
 
 
 const pool = new pg.Pool(configs);
